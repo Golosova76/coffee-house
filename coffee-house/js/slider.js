981ac8f6387acde3789e1bf1723a01e9352788e3
Anna Golosova <golosova76@gmail.com>
2023-12-05 16:00:12 +0300

    feat: added js functionality slider progress bar atomatic

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -5,11 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const slides = wrapper.querySelectorAll('.slider__item.content');
   const prevButton = document.querySelector('.slider__button-prev');
   const nextButton = document.querySelector('.slider__button-next');
-  const progressBar = document.querySelectorAll('.slider__pagination .slider__line');  
+  const progressBar = document.querySelectorAll('.slider__pagination .slider__line');
   const sliderInterval = 5000; //автоматперелистывание 5 сек (на время верстки 2 сек)
+  const progressIntervalStep = 100;
   let slideWidth = slides[0].offsetWidth;
   let currentSlide = 0;
-  let autoSlidernterval;
+  let autoSliderInterval;
   let progressInterval;
     
   function getSlideOffset(index) {
@@ -19,8 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
   function moveSlider(slideIndex) {
     const offset = getSlideOffset(slideIndex);
     wrapper.style.transform = `translateX(${offset}px)`;
+    resetProgressBar(currentSlide);
     currentSlide = slideIndex;
-    resetProgressBar();
+    startProgressBar();
   }
 
   function moveNext() {
@@ -46,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function startAutoSlider() {
-    autoSlidernterval = setInterval(autoSlider, sliderInterval);
+    autoSliderInterval = setInterval(autoSlider, sliderInterval);
     startProgressBar();
   }
 
@@ -54,27 +56,35 @@ document.addEventListener("DOMContentLoaded", function() {
     clearInterval(autoSliderInterval);
     clearInterval(progressInterval);
     resetProgressBar();
+    startAutoSlider();
   }
 
   function startProgressBar() {
-    //progressBar[currentSlide].style.width = '100%';
-    progressBar[currentSlide].style.backgroundColor = '#665F55';
     let progress = 0;
-    progressInterval = setInterval(() => {
-      progress += (100 / (sliderInterval / 100));
+    let startTime;
+    
+    function animate() {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - startTime;
+
+      progress += (elapsedTime / (sliderInterval / progressIntervalStep));
+
       progressBar[currentSlide].style.background = `linear-gradient(to right, #665F55 ${Math.min(progress, 100)}%, #C1B6AD ${Math.min(progress, 100)}%)`;
-      if (progress >= 100) {
-        clearInterval(progressInterval);
-        progressBar[currentSlide].style.background = '#C1B6AD';
+
+      if (progress < 100) {
+          startTime = currentTime;
+          requestAnimationFrame(animate);
+      } else {
+          resetProgressBar();
       }
-    }, 100);
+    }
+
+    startTime = Date.now();
+    requestAnimationFrame(animate);
   }
 
-  function resetProgressBar() {
-    clearInterval(progressInterval);
-    //progressBar[currentSlide].style.width = '40px';
-    progressBar[currentSlide].style.backgroundColor = '#C1B6AD';
-    startProgressBar();
+  function resetProgressBar(slideIndex) {
+    progressBar[currentSlide].style.background = '#C1B6AD';
   }
 
   nextButton.addEventListener('click', moveNext);
