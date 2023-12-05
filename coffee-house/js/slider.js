a7b0acbe2434a29b1ffe9a844a0d41ac5f78aeb8
Anna Golosova <golosova76@gmail.com>
2023-12-05 12:23:57 +0300

    feat: added js functionality slider progress bar

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
   const slides = wrapper.querySelectorAll('.slider__item.content');
   const prevButton = document.querySelector('.slider__button-prev');
   const nextButton = document.querySelector('.slider__button-next');
+  const progressBar = document.querySelectorAll('.slider__pagination .slider__line');  
+  const sliderInterval = 5000; //автоматперелистывание 5 сек (на время верстки 2 сек)
   let slideWidth = slides[0].offsetWidth;
   let currentSlide = 0;
   let autoSlidernterval;
-  
+  let progressInterval;
+    
   function getSlideOffset(index) {
     return -(index * slideWidth);
   }
@@ -17,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const offset = getSlideOffset(slideIndex);
     wrapper.style.transform = `translateX(${offset}px)`;
     currentSlide = slideIndex;
+    resetProgressBar();
   }
 
   function moveNext() {
@@ -25,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       moveSlider(0);
     }
+    resetAutoSlider();
   }
 
   function movePrev() {
@@ -33,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       moveSlider(slides.length - 1);
     }
+    resetAutoSlider();
   }
 
   function autoSlider() {
@@ -40,20 +46,43 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function startAutoSlider() {
-    autoSlidernterval = setInterval(autoSlider, 2000); //автоматперелистывание 5 сек (на время верстки 2 сек)
+    autoSlidernterval = setInterval(autoSlider, sliderInterval);
+    startProgressBar();
   }
 
   function resetAutoSlider() {
-    clearInterval(autoSlidernterval);
-    startAutoSlider();
+    clearInterval(autoSliderInterval);
+    clearInterval(progressInterval);
+    resetProgressBar();
   }
 
-  startAutoSlider();
+  function startProgressBar() {
+    //progressBar[currentSlide].style.width = '100%';
+    progressBar[currentSlide].style.backgroundColor = '#665F55';
+    let progress = 0;
+    progressInterval = setInterval(() => {
+      progress += (100 / (sliderInterval / 100));
+      progressBar[currentSlide].style.background = `linear-gradient(to right, #665F55 ${Math.min(progress, 100)}%, #C1B6AD ${Math.min(progress, 100)}%)`;
+      if (progress >= 100) {
+        clearInterval(progressInterval);
+        progressBar[currentSlide].style.background = '#C1B6AD';
+      }
+    }, 100);
+  }
+
+  function resetProgressBar() {
+    clearInterval(progressInterval);
+    //progressBar[currentSlide].style.width = '40px';
+    progressBar[currentSlide].style.backgroundColor = '#C1B6AD';
+    startProgressBar();
+  }
 
   nextButton.addEventListener('click', moveNext);
   prevButton.addEventListener('click', movePrev);
   
+  startAutoSlider();
 
 
   //DON'T TOUCH!!!!!
 });
+
